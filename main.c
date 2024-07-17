b538d7ab2e4784432f5123bd3aacadbf7ec68252
Oleksiy Zaluzhnyi <alexzintech@gmail.com>
2024-07-17 14:24:28 +0300

    Add some data structures and constants

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    char name[50];
+    float price;
+    int stock;
+} Product;
+
+typedef struct {
+    char name[50];
+    float price;
+    int quantity;
+} CartItem;
+
+#define MAX_PRODUCTS 10
+#define MAX_CART_ITEMS 10
+
 int main() {
   return 1;
 }
