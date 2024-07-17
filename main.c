84e16bad171efdde1127cbb422b43c96b8765d95
Oleksiy Zaluzhnyi <alexzintech@gmail.com>
2024-07-17 14:28:46 +0300

    Add Products and Cart arrays

--- main.c
+++ main.c
@@ -17,6 +17,17 @@ typedef struct {
 #define MAX_PRODUCTS 10
 #define MAX_CART_ITEMS 10
 
+Product products[MAX_PRODUCTS] = {
+    {"Whey Protein", 29.99, 10},
+    {"Casein Protein", 34.99, 5},
+    {"Soy Protein", 24.99, 8},
+    {"Pea Protein", 27.99, 7},
+    {"Egg Protein", 31.99, 6}
+};
+
+CartItem cart[MAX_CART_ITEMS];
+int cartSize = 0;
+
 int main() {
   return 1;
 }
