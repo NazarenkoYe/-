4427a90c9830ba134ee2c7fa6d0d88dbf4b87a60
AlexBeLike <alexzintech@gmail.com>
2024-07-17 13:57:39 +0300

    Add a menu

--- main.c
+++ main.c
@@ -3,5 +3,11 @@
 #include <string.h>
 
 int main() {
-    printf("Welcome to Codeblocks");
+    int choice, productIndex, quantity, cartIndex;
+
+    while (1) {
+        printf("\n1. Display Products\n2. Add to Cart\n3. Remove from Cart\n4. View Cart\n5. Checkout\n6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+}
 }
